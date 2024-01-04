1ad1d5991b25780b40a05af732d7378186767a39
Harsh Poddar <harshvpoddar1605@gmail.com>
2024-01-04 23:47:21 +0530

    Update roles and names in humans.txt and Home.js

--- src/layouts/Home/Home.js
+++ src/layouts/Home/Home.js
@@ -18,7 +18,7 @@ import { ProjectSummary } from 'layouts/Home/ProjectSummary';
 import { useEffect, useRef, useState } from 'react';
 import styles from './Home.module.css';
 
-const disciplines = ['Developer', 'Prototyper', 'Animator', 'Illustrator', 'Modder'];
+const disciplines = ['Student', 'ML Engineer','Researcher'];
 
 export const Home = () => {
   const [visibleSections, setVisibleSections] = useState([]);
@@ -68,7 +68,7 @@ export const Home = () => {
   return (
     <div className={styles.home}>
       <Meta
-        title="Designer + Developer"
+        title="Developer + ML Engineer"
         description="Design portfolio of Hamish Williams — a product designer working on web & mobile
           apps with a focus on motion, experience design, and accessibility."
       />
